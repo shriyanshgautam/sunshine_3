986c57557c33417844a9a8b9c9fa74edee98d59a
Shriyansh Gautam <shriyansh.gautam.cse13@iitbhu.ac.in>
2015-03-10 11:47:22 +0530

    Added ListItem Click Listner and Toast the content

--- Sunshine/app/src/main/java/com/shriyansh/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/shriyansh/sunshine/ForecastFragment.java
@@ -13,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -88,6 +90,13 @@ public class ForecastFragment extends Fragment {
         forecastAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
         ListView listView=(ListView)rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(),forecastAdapter.getItem(position),Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
